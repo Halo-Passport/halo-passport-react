d21aace58ebbc0de72009c1d61b1fefe1ac3ae36
modiejun <junjielin8888@gmail.com>
2021-01-31 10:50:00 +0800

    Update Contract - > front end listen on event

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+// import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import TestingCenter from "./contracts/TestingCenter.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
-
+  state = { storageValue: 0, web3: null, accounts: null, contract: null ,subEvent:null};
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
@@ -28,7 +27,7 @@ class App extends Component {
         TestingCenter.abi, 
         deployedNetwork && deployedNetwork.address,
       )
-
+      
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
       this.setState({ web3, accounts, contract: instance }, this.runExample);
@@ -40,19 +39,50 @@ class App extends Component {
       console.error(error);
     }
   };
+  
 
   runExample = async () => {
-    const { accounts, contract } = this.state;
+    const { web3,accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
     // await contract.methods.set(10).send({ from: accounts[0] });
 
     // Get the value from the contract to prove it worked.
     const response = await contract.methods.name().call();
+    console.log(contract.methods.name().call())
+
+    const result = await contract.getPastEvents(
+      "allEvents",
+      {fromBlock:0,toBlock:'latest'},
+      function(error,events){ 
+        console.log(events); })
+      .then(function(events){
+
+          console.log(events[events.length-1].returnValues) // same results as the optional callback above
+          return JSON.stringify(events[events.length-1].returnValues)
+      }
+    );
+
 
     // Update state with the result.
-    this.setState({ storageValue: response });
+    this.setState({ storageValue: response, subEvent:result });
   };
+
+  refreshEvents = async()=>{
+    const { web3,accounts, contract } = this.state;
+    const result = await contract.getPastEvents(
+      "allEvents",
+      {fromBlock:0,toBlock:'latest'},
+      function(error,events){ 
+        console.log(events); })
+      .then(function(events){
+
+          console.log(events[events.length-1].returnValues) // same results as the optional callback above
+          return JSON.stringify(events[events.length-1].returnValues)
+      }
+    );
+    this.setState({  subEvent:result });
+  }
 
   render() {
     if (!this.state.web3) {
@@ -71,6 +101,8 @@ class App extends Component {
           Try changing the value stored on <strong>line 40</strong> of App.js.
         </p>
         <div>The stored value is: {this.state.storageValue}</div>
+        <div>The subscribed Event listner {this.state.subEvent}</div>
+        <button onClick={this.refreshEvents} >Refresh</button>
       </div>
     );
   }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,5 @@
-var TestingCenter = artifacts.require("./TestingCenter.sol");
+// var TestingCenter = artifacts.require("./TestingCenter.sol");
+var SimpleStorage = artifacts.require("./SimpleStorage.sol")
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  // deployer.deploy(SimpleStorage);
 };
